2978f019e0e0d7876c161c2ae76e814215ad2f6f
Sudipta paul <Sudiptopaul403@gmail.com>
2023-07-15 10:45:57 +0530

    datachat.py

--- datachat.py
+++ datachat.py
@@ -1,24 +1,17 @@
-import streamlit as st
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import os
+import streamlit as st
+import tempfile
 import pandas as pd
-from pandasai import PandasAI
 
 OPENAI_API_KEY= st.secrets["auth_token"]
 
-def chat_with_csv(df,prompt):
-     llm = OpenAI(temperature=0,openai_api_key="OPENAI_API_KEY")
-     pandas_ai = PandasAI(llm)
-     result = pandas_ai.run(df,prompt=prompt)
-     print(result)
-     return result
-
-
 def main():
     load_dotenv()
-     # Load the OpenAI API key from the environment variable
+
+    # Load the OpenAI API key from the environment variable
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
@@ -26,7 +19,7 @@ def main():
         print("OPENAI_API_KEY is set")
 
     st.set_page_config(page_title="AskCSV",page_icon=":robot:",layout="wide")
-    
+
     #CSS
     hide_st_style = """
             <style>
@@ -48,25 +41,35 @@ def main():
         """
     st.markdown(bg, unsafe_allow_html=True)
     
-    st.header("Ask your CSV 💬")
+    st.header("Ask your CSV 📊")
 
-    user_csv = st.file_uploader("Upload your CSV file", type ="csv")
-
-    if user_csv is not None:
+    csv_file = st.file_uploader("Upload a CSV file", type="csv")
+    if csv_file is not None:
         col1,col2 = st.columns([1,1])
         with col1:
-           #user_question = st.text_input("Ask a question about your CSV:")
-            data = pd.read_csv(user_csv)
+        # Save the uploaded file to a temporary location
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(csv_file.read())
+                csv_file_path = temp_file.name
+            
+            # Read the CSV file
+            data = pd.read_csv(csv_file_path)
+
+            # Display the data frame on the left side
+            st.info("CSV Dataset")
             st.dataframe(data)
-
+        
         with col2:
-            st.info("Chat with your CSV")
 
+            st.info("Chat with your CSV")
             input_text = st.text_input("Enter your Query")
             if input_text is not None:
-                if st.button("Chat with CSV"):
-                    st.info("Your query: "+input_text)
-                    result = chat_with_csv(data,input_text)
-                    st.success(result)
+                if st.button("chat with csv"):
+                    agent = create_csv_agent(OpenAI(temperature=0,openai_api_key="OPENAI_API_KEY"), csv_file_path, verbose=True)
+                    with st.spinner(text="In progress..."):
+                        st.info("Your query: "+input_text)
+                        st.write(agent.run(input_text))
 
-main()
+
+if __name__ == "__main__":
+    main()
